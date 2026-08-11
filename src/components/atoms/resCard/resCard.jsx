1e2ec86d463fd82bc27eb0b003adad5cd352784e
FocUUsMe <timoxxastolarov@gmail.com>
2026-08-11 21:58:17 +0300

    smallDebug

--- src/components/atoms/resCard/resCard.jsx
+++ src/components/atoms/resCard/resCard.jsx
@@ -9,28 +9,18 @@
                     <img
                         src={img}
                         alt='resource_image'
-<<<<<<< HEAD
                         className='res_img'
                         loading='eager'
-=======
-                        className='resource_card_img'
->>>>>>> feature/frontend
                     />
 
                     <h2 className='resources_card_title'> {title} </h2>
                     <p className='resources_card_desc'> {descript} </p>
                 </div>
 
-<<<<<<< HEAD
                 <h3 className='res_title'> {title} </h3>
                 <p className='res_desc'> {descript} </p>
 
                 <Link className='res_card_link' href={link}> {"See >"} </Link>
-=======
-                <Link className='resource_card_link' href={link}> 
-                    {"Watch >"} 
-                </Link>
->>>>>>> feature/frontend
             </div>
         )
     }
--- src/components/atoms/vacanCard/vacanCard.jsx
+++ src/components/atoms/vacanCard/vacanCard.jsx
@@ -11,31 +11,15 @@
                         <h2> {title} </h2>
                         <p> {pay} </p>
                     </div>
-            <div className="vac_card">
-                <div className='card_group'>
-                    <Image
-                        src={'/next.svg'} // will be changed to {logo}
-                        alt='vacancie card'
-                        width={300}
-                        height={150}
-                        className='card_img'
-                        loading='eager'
-                    />
-
-                    <h3 className='card_title'> {title} </h3>
-                    <p className='card_pay'> {pay} </p>
-                </div>
-
                 <Link 
                     href={link} 
                     className='vacancies_preview_card_btn'
-                > 
-                    Check vacancie {">"} 
-                <Link className='vac_card_link' href={link}>
-                        {"See >"}
-                </Link>
+                    
+                > Check vacancie {">"} </Link>
+                
+                </div>
             </div>
-        )    
+        )
     }
 
     export default VacancieCard;
