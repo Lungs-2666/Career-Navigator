d56b77c3d95d214a58bf0b6e1b0d23daec7c42f5
Chin-Giz Priymak <primak2020@gmail.com>
2026-08-12 15:14:00 +0300

    del skillMap link in navBar

--- src/components/molecules/navBar/navBar.jsx
+++ src/components/molecules/navBar/navBar.jsx
@@ -9,7 +9,6 @@
                     <Link href='/' className='nav_item' > <House color='#191314' size={24} strokeWidth={1.5} /> </Link>
                     <Link href='/vacancies' className="nav_item" > <BriefcaseBusiness color='#191314' size={24} strokeWidth={1.5}/> </Link>
                     <Link href='/resources' className="nav_item" > <BookA color='#191314' size={24} strokeWidth={1.5}/>             </Link>
-                    <Link href='/skillmap' className="nav_item" >  <MapPinned color='#191314' size={24} strokeWidth={1.5}/>         </Link>
                 </div>
 
                 <div className='nav_bar_2'>
