c0aa4193c23dfc20eca8512120806a879432e4b6
Chin-Giz Priymak <primak2020@gmail.com>
2026-08-14 21:59:01 +0300

    create branch feature/front-back

--- src/app/vacancies/page.jsx
+++ src/app/vacancies/page.jsx
@@ -1,6 +1,70 @@
+
+
 import styles from './page.module.css'
+import VacancieCard from '@/components/atoms/vacanCard/vacanCard';
+
 
 const Page = () => {
+
+
+    const test_cards = [
+        {
+            id: 1,
+            title: 'card_test',
+            pay: '$100/month',
+            logo: '/logos/logo_1',
+            link: '/vacancie/1'
+        },
+        {
+            id: 2,
+            title: 'card_test',
+            pay: '$100/month',
+            logo: '/logos/logo_1',
+            link: '/vacancie/1'
+        },
+        {
+            id: 3,
+            title: 'card_test',
+            pay: '$100/month',
+            logo: '/logos/logo_1',
+            link: '/vacancie/1'
+        },
+        {
+            id: 4,
+            title: 'card_test',
+            pay: '$100/month',
+            logo: '/logos/logo_1',
+            link: '/vacancie/1'
+        },
+        {
+            id: 5,
+            title: 'card_test',
+            pay: '$100/month',
+            logo: '/logos/logo_1',
+            link: '/vacancie/1'
+        },
+        {
+            id: 6,
+            title: 'card_test',
+            pay: '$100/month',
+            logo: '/logos/logo_1',
+            link: '/vacancie/1'
+        },
+        {
+            id: 7,
+            title: 'card_test',
+            pay: '$100/month',
+            logo: '/logos/logo_1',
+            link: '/vacancie/1'
+        },
+        {
+            id: 8,
+            title: 'card_test',
+            pay: '$100/month',
+            logo: '/logos/logo_1',
+            link: '/vacancie/1'
+        }
+    ]
 
     return (
         <div className={styles.page}
@@ -20,10 +84,18 @@ const Page = () => {
                     <button className={styles.vacanciesInputBtn}>Найти</button>
                 </div>
             </header>
-            <main   className={styles.vacanciesMain}>
-                <div>
-                    card
-                </div>
+            <main className={styles.vacanciesMain}>
+                {test_cards.map((item) => {
+                    return (
+                        <VacancieCard
+                            key={item.id}
+                            logo={item.logo}
+                            title={item.title}
+                            pay={item.pay}
+                            link={item.link}
+                        />
+                    );
+                })}
             </main>
         </div>
     )
