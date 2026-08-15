0e361fe479b1cf97b65ad397516c385dcd79d3aa
Chin-Giz Priymak <primak2020@gmail.com>
2026-08-15 23:38:17 +0300

    add dynamic render of the skills list in vacancie card

--- src/components/vacancieCard/vacancieCard.jsx
+++ src/components/vacancieCard/vacancieCard.jsx
@@ -3,6 +3,10 @@ import './vacancieCard.css'
 
 const VacancieCard = ({ title, company, salaryMin, salaryMax, source, currency, description, skills, url, createdAt }) => {
 
+
+    const visibleSkills = skills.length > 2 ? skills.slice(0, 2) : skills;
+    const moreSkills = skills.length > 2 ? skills.length - 2 : 0;
+
     return (
         <div className="vacancieCard">
             <div className="vacancieCardHeader">
@@ -11,9 +15,23 @@ const VacancieCard = ({ title, company, salaryMin, salaryMax, source, currency, 
             </div>
             <div className="vacancieCardMain">
                 <div className="vacancieCardSkillsContainer">
-                    <div className="vacancieCardSkill">{skills[0]}</div>
+                    {visibleSkills.map((skill) => {
+                        return (
+                            <span
+                                key={skill}
+                                className='vacancieCardSkill'
+                            >{skill}</span>
+                        )
+                    })}
+                    {moreSkills > 0 && (
+                        <span
+                            key={'more'}
+                            className='vacancieCardSkillMore'
+                        >+{moreSkills}</span>
+                    )}
+                    {/* <div className="vacancieCardSkill">{skills[0]}</div>
                     <div className="vacancieCardSkill">{skills[1]}</div>
-                    <div className="vacancieCardSkill">+2</div>
+                    <div className="vacancieCardSkill">+2</div> */}
                 </div>
                 <div className="vacancieCardSalaryContainer">
                     <p className="vacancieCardSalary">{`${salaryMin} - ${salaryMax}`}</p>
