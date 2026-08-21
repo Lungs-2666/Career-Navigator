faadec963f0e7923b4799093be316062007a6e13
Chin-Giz Priymak <primak2020@gmail.com>
2026-08-21 10:09:02 +0300

    add data in db

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,34 +1,46 @@
-console.log('0 file loaded')
 import { connectToDatabase } from "../src/lib/mongo.js";
 import fs from 'fs'
 import { dirname, join } from 'path'
 import { fileURLToPath } from "url";
 
-console.log('1 imports loaded')
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-const file = join(__dirname, '../data/vacancies.json')
-console.log(' 2 создали файл ')
+const fileVacancies = join(__dirname, '../data/vacancies.json')
+const fileSkills = join(__dirname, '../data/skills.json')
 
 
 async function main() {
   console.log('start')
 
-  console.log('читаем файл')
-  let raw = fs.readFileSync(file, 'utf8')
+  console.log('читаем файл vacancies')
+  let rawVac = fs.readFileSync(fileVacancies, 'utf8')
   console.log(' файл прочитан')
 
-  console.log("парсим файл")
-  let list = JSON.parse(raw)
-  console.log('json запаршен')
-  console.log('подключаемсся к бд')
+  console.log("парсим файл vacancies")
+  let listVac = JSON.parse(rawVac)
+
+
+  console.log('читаем файл skills')
+  let rawSkills = fs.readFileSync(fileSkills, 'utf8')
+  console.log(' файл прочитан')
+
+  console.log("парсим файл skills")
+  let listSkll = JSON.parse(rawSkills)
+
+  console.log("подключение к бд")
   let { db, client } = await connectToDatabase();
-  console.log('подключились к бд ')
+
+  console.log('начало записи vacancies')
   await db.collection('vacancies').deleteMany({})
-  await db.collection('vacancies').insertMany(list)
-  console.log('Добавленно записей:', list.length)
+  await db.collection('vacancies').insertMany(listVac)
+  console.log('Добавленно записей vacancies:', listVac.length)
+
+  console.log('начало записи skills')
+  await db.collection('skills').deleteMany({})
+  await db.collection('skills').insertMany(listSkll)
+  console.log('Добавленно записей skills:', listSkll.length)
 
   await client.close()
   process.exit(0)
